c76d87ee28a6234db078d6193f2187237d005cab
Pratiksha Chavan <pratikshakchavan123@gmail.com>
2026-06-16 20:54:37 +0530

    Update 80_streamlit_Input.py

--- Generative AI/80_streamlit_Input.py
+++ Generative AI/80_streamlit_Input.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Marvellous Infosystems by Piyush Manohar Khairnar")
+st.title("Programming by Pratiksha Kalyan Chavan")
 
 name = st.text_input("Enter your name")
 
