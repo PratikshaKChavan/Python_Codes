d53d438c41853b14a0949c1a2448ce57d3d6a3d3
Pratiksha Chavan <pratikshakchavan123@gmail.com>
2026-06-16 20:55:30 +0530

    Update 81_streamlit_Button.py

--- Generative AI/81_streamlit_Button.py
+++ Generative AI/81_streamlit_Button.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Marvellous Infosystems by Piyush Manohar Khairnar")
+st.title("Programming by Pratiksha Kalyan Chavan")
 
 if st.button("Marvellous_Python"):
     st.success("Jay Ganesh")
